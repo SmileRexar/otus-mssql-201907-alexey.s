adce14c3f263df001d655a973a956f377f898198
alexey <dfds>
2019-08-15 06:29:00 +0000

    Промежуточные задания 3 и 5

--- HW02/Выборки.sql
+++ HW02/Выборки.sql
@@ -22,3 +22,43 @@ FROM            Sales.Customers AS c LEFT JOIN
                          Sales.Orders AS o ON c.CustomerID = o.CustomerID
 WHERE        (o.CustomerID IS NULL)
 
+/*
+3. Продажи с названием месяца, в котором была продажа, номером квартала, к которому относится продажа, 
+включите также к какой трети года относится дата - каждая треть по 4 месяца, 
+дата забора заказа должна быть задана, с ценой товара более 100$ либо количество единиц товара более 20. 
+Добавьте вариант этого запроса с постраничной выборкой пропустив первую 1000 и отобразив следующие 100 записей. 
+Соритровка должна быть по номеру квартала, трети года, дате продажи. 
+*/
+
+/*
+4. Заказы поставщикам, которые были исполнены за 2014й год с доставкой Road Freight или Post, 
+добавьте название поставщика, имя контактного лица принимавшего заказ
+*/
+SELECT        o.OrderID, Sales.Customers.CustomerName, p.FullName, d.DeliveryMethodName
+FROM            Sales.Orders AS o INNER JOIN
+                         Sales.Customers ON o.CustomerID = Sales.Customers.CustomerID INNER JOIN
+                         Application.People AS p ON o.SalespersonPersonID = p.PersonID INNER JOIN
+                         Sales.Invoices AS i ON i.OrderID = o.OrderID INNER JOIN
+                         Application.DeliveryMethods AS d ON i.DeliveryMethodID = d.DeliveryMethodID
+WHERE        (YEAR(o.OrderDate) = '2014') AND (d.DeliveryMethodName = N'POST') OR
+                         (d.DeliveryMethodName = N'Road Freight')
+
+
+/*
+5. 10 последних по дате продаж с именем клиента и именем сотрудника, который оформил заказ.
+*/
+SELECT        TOP (10) o.OrderID, c.CustomerName, p.FullName, o.OrderDate
+FROM            Sales.Orders AS o INNER JOIN
+                         Sales.Customers AS c ON o.CustomerID = c.CustomerID INNER JOIN
+                         Application.People AS p ON o.SalespersonPersonID = p.PersonID
+ORDER BY o.OrderDate DESC
+
+SELECT        TOP (10) o.OrderID, c.CustomerName, p.FullName, o.OrderDate
+FROM            Sales.Orders AS o INNER JOIN
+                         Sales.Customers AS c ON o.CustomerID = c.CustomerID INNER JOIN
+                         Application.People AS p ON o.SalespersonPersonID = p.PersonID
+ORDER BY  convert(datetime, o.OrderDate, 103) DESC
+
+/*
+6. Все ид и имена клиентов и их контактные телефоны, которые покупали товар Chocolate frogs 250g
+*/
