6b7f77bea5b4cfb74e4b8abb4af77d79bdc4a9e2
alexey <53871193+SmileRexar@users.noreply.github.com>
2019-11-03 10:05:16 +0000

    Файлы проекта clr

---
+++ HW14/ExternalSQLHelper/ExternalSQLHelper/SQLHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.SqlTypes;
+using System.Text.RegularExpressions;
+
+    public class SQLHelper
+    {
+        public static bool IsIpAdpress(SqlString strName)
+        {
+            Match match = Regex.Match(strName.ToString(), @"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}");
+            if (match.Success)
+                return true;
+
+            return false;
+        }
+    }
+ 
