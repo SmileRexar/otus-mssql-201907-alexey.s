fd1360a626da80764dbb060dd3962f0b1dcf4f79
alexey <53871193+SmileRexar@users.noreply.github.com>
2019-12-08 13:14:12 +0000

    Отслеживание хода выполнения с помощью xEvent:

---
+++ HW31(Резервирование)/Лекции/BackupNRestore_2.sql
@@ -0,0 +1,20 @@
+﻿
+--Отслеживание хода выполнения с помощью xEvent:
+-- Create the backup_restore_progress_trace extended event esssion
+CREATE EVENT SESSION [BackupRestoreTrace] ON SERVER 
+ADD EVENT sqlserver.backup_restore_progress_trace
+ADD TARGET package0.event_file(SET filename=N'BackupRestoreTrace')
+WITH (MAX_MEMORY=4096 KB,EVENT_RETENTION_MODE=ALLOW_SINGLE_EVENT_LOSS,MAX_DISPATCH_LATENCY=5 SECONDS,MAX_EVENT_SIZE=0 KB,MEMORY_PARTITION_MODE=NONE,TRACK_CAUSALITY=OFF,STARTUP_STATE=OFF)
+GO
+
+-- Start the event session  
+ALTER EVENT SESSION [BackupRestoreTrace]  
+ON SERVER  
+STATE = start;  
+GO  
+
+-- Stop the event session  
+ALTER EVENT SESSION [BackupRestoreTrace]  
+ON SERVER  
+STATE = stop;  
+GO  
