b21dd441316a93c1cb57318b8a87a46afffb5774
alexey <53871193+SmileRexar@users.noreply.github.com>
2019-12-22 19:41:57 +0000

    1 Часть 7 ДЗ

--- HW07(SQL Window Functions)/Select.sql
+++ HW07(SQL Window Functions)/Select.sql
@@ -14,7 +14,59 @@
 2015-02-03	9626342.98
 Продажи можно взять из таблицы Invoices.
 Нарастающий итог должен быть без оконной функции.
- 
+*/
+--  Сам запрос
+SELECT i.InvoiceID, c.CustomerName, i.CustomerID, i.InvoiceDate,
+	SUM(il.Quantity * il.UnitPrice) OVER (PARTITION BY i.InvoiceID),
+	SUM(il.Quantity * il.UnitPrice) OVER (ORDER BY DATEPART(YEAR, i.InvoiceDate), DATEPART(MONTH, i.InvoiceDate))
+FROM Sales.Invoices i
+INNER JOIN Sales.Customers c ON c.CustomerID = i.CustomerID
+INNER JOIN Sales.InvoiceLines il ON il.InvoiceID = i.InvoiceID
+WHERE i.InvoiceDate >= '20150101' 
+ORDER BY i.InvoiceDate, i.CustomerID;
+
+-- Проверка работы
+set statistics time on 
+-- запись в временную таблицу
+DROP TABLE IF EXISTS tempdb.dbo.#TempT
+SELECT i.InvoiceID, c.CustomerName, i.InvoiceDate,
+	SUM(il.Quantity * il.UnitPrice) OVER (PARTITION BY i.InvoiceID) as Sum1,
+	SUM(il.Quantity * il.UnitPrice) OVER (ORDER BY DATEPART(YEAR, i.InvoiceDate), DATEPART(MONTH, i.InvoiceDate)) as Sum2
+into #TempT
+FROM Sales.Invoices i
+INNER JOIN Sales.Customers c ON c.CustomerID = i.CustomerID
+INNER JOIN Sales.InvoiceLines il ON il.InvoiceID = i.InvoiceID
+WHERE i.InvoiceDate >= '20150101' 
+ORDER BY i.InvoiceDate, i.CustomerID;
+select * from #TempT
+
+DECLARE @Temp2 TABLE(
+    InvoiceID nvarchar(150) NOT NULL,
+    CustomerName nvarchar(150) NOT NULL, 
+	InvoiceDate datetime NOT NULL,
+	Sum1 int NOT NULL,
+	Sum2 int NOT NULL
+);
+
+-- запись в табличную переменную
+INSERT INTO @Temp2 (InvoiceID, CustomerName, InvoiceDate, Sum1, Sum2)
+SELECT i.InvoiceID, c.CustomerName, i.InvoiceDate,
+	SUM(il.Quantity * il.UnitPrice) OVER (PARTITION BY i.InvoiceID) as Sum1,
+	SUM(il.Quantity * il.UnitPrice) OVER (ORDER BY DATEPART(YEAR, i.InvoiceDate), DATEPART(MONTH, i.InvoiceDate)) as Sum2
+FROM Sales.Invoices i
+INNER JOIN Sales.Customers c ON c.CustomerID = i.CustomerID
+INNER JOIN Sales.InvoiceLines il ON il.InvoiceID = i.InvoiceID
+WHERE i.InvoiceDate >= '20150101' 
+ORDER BY i.InvoiceDate, i.CustomerID;
+select * from @Temp2
+
+/*
+Для количества записей в dataset = 101356 
+Вставка в временную таблицу 38% против вставки в табличную переменную 61% согласно актуальному плану выполнения
+*/
+
+
+/*
 2. Если вы брали предложенный выше запрос, то сделайте расчет суммы нарастающим итогом с помощью оконной функции.
 Сравните 2 варианта запроса - через windows function и без них. Написать какой быстрее выполняется, сравнить по set statistics time on;
 2. Вывести список 2х самых популярных продуктов (по кол-ву проданных) в каждом месяце за 2016й год (по 2 самых популярных продукта в каждом месяце)
@@ -36,3 +88,4 @@
 Опционально можно сделать вариант запросов для заданий 2,4,5 без использования windows function и сравнить скорость как в задании 1. 
 
 */
+
