055295064fd104e67d3226251c154479bda0874f
alexey <53871193+SmileRexar@users.noreply.github.com>
2019-12-24 20:46:11 +0000

    Добавлено 1 задание из ДЗ

--- HW08/Select.sql
+++ HW08/Select.sql
@@ -43,3 +43,56 @@ CountryId	CountryName	Code
 Что делает запрос? 
 Чем можно заменить CROSS APPLY - можно ли использовать другую стратегию выборки\запроса?
 */
+
+/*
+SELECT *
+FROM
+(
+    SELECT format([InvoiceDate], 'MM-yyyy') AS Data1, 
+           c.[CustomerName] AS [CustomerName], 
+           COUNT(*) AS [Count]
+    FROM [WideWorldImporters].[Sales].[Invoices] AS i
+         INNER JOIN [Sales].[Customers] AS c ON i.CustomerID = c.CustomerID
+    WHERE i.CustomerID BETWEEN 2 AND 6
+    GROUP BY format([InvoiceDate], 'MM-yyyy'), 
+             c.[CustomerName]
+) AS Cl PIVOT(SUM([Count]) FOR [CustomerName] IN([Tailspin Toys (Sylvanite, MT)], 
+                                                 [Tailspin Toys (Peeples Valley, AZ)], 
+                                                 [Tailspin Toys (Medicine Lodge, KS)], 
+                                                 [Tailspin Toys (Gasport, NY)], 
+                                                 [Tailspin Toys (Jessie, ND)])) AS PVT
+*/
+
+/*
+Pivot и Cross Apply
+1. Требуется написать запрос, который в результате своего выполнения формирует таблицу следующего вида:
+Название клиента
+МесяцГод Количество покупок
+
+Клиентов взять с ID 2-6, это все подразделение Tailspin Toys
+имя клиента нужно поменять так чтобы осталось только уточнение 
+например исходное Tailspin Toys (Gasport, NY) - вы выводите в имени только Gasport,NY
+дата должна иметь формат dd.mm.yyyy например 25.12.2019
+
+Например, как должны выглядеть результаты:
+InvoiceMonth	Peeples Valley, AZ	Medicine Lodge, KS	Gasport, NY	Sylvanite, MT	Jessie, ND
+01.01.2013	3	1	4	2	2
+01.02.2013	7	3	4	2	1
+*/
+SELECT *
+FROM
+(
+    SELECT DATEADD(day, -DAY(i.InvoiceDate) + 1, i.InvoiceDate) AS InvoiceMonth, 
+           SUBSTRING(c.CustomerName, CHARINDEX('(', c.CustomerName) + 1, CHARINDEX(')', c.CustomerName) - CHARINDEX('(', c.CustomerName) - 1) AS CustomerName, 
+           i.InvoiceID
+    FROM Sales.Customers AS c
+         JOIN Sales.Invoices AS i ON c.CustomerID = i.CustomerID
+    WHERE c.CustomerID >= 2
+          AND c.CustomerID <= 6
+) AS s PIVOT(COUNT(s.InvoiceID) FOR CustomerName IN([Sylvanite, MT], 
+                                                    [Peeples Valley, AZ], 
+                                                    [Medicine Lodge, KS], 
+                                                    [Gasport, NY], 
+                                                    [Jessie, ND])) AS PVT
+ORDER BY InvoiceMonth;
+
