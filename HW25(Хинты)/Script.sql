3cd0406e7200da1565749aaf97d8b28ff11eae7f
alexey <53871193+SmileRexar@users.noreply.github.com>
2019-12-24 20:22:55 +0000

    Добавлено: 1) описание с set statistics io on 2) создание доп поля в индексе таблицы Invoices для производительности

--- HW25(Хинты)/Script.sql
+++ HW25(Хинты)/Script.sql
@@ -8,6 +8,7 @@
 Вариант 2. Оптимизируйте запрос по БД WorldWideImporters. 
 Приложите текст запроса со статистиками по времени и операциям ввода вывода, опишите кратко ход рассуждений при оптимизации. 
 */
+
 Select ord.CustomerID, det.StockItemID, SUM(det.UnitPrice), SUM(det.Quantity), COUNT(ord.OrderID)	FROM Sales.Orders AS ord JOIN Sales.OrderLines AS det ON det.OrderID = ord.OrderID 
 JOIN Sales.Invoices AS Inv ON Inv.OrderID = ord.OrderID JOIN Sales.CustomerTransactions AS Trans ON Trans.InvoiceID = Inv.InvoiceID JOIN Warehouse.StockItemTransactions AS ItemTrans 
 ON ItemTrans.StockItemID = det.StockItemID WHERE Inv.BillToCustomerID != ord.CustomerID AND (Select SupplierId FROM Warehouse.StockItems AS It Where It.StockItemID = det.StockItemID) = 12 
@@ -24,6 +25,7 @@ AND (SELECT SUM(Total.UnitPrice*Total.Quantity) FROM Sales.OrderLines AS Total J
 /*
 Переписал на читаемый вид
 */
+set statistics io on
 Select ord.CustomerID, det.StockItemID, SUM(det.UnitPrice), SUM(det.Quantity), COUNT(ord.OrderID)	
 FROM Sales.Orders AS ord JOIN Sales.OrderLines AS det ON det.OrderID = ord.OrderID 
 JOIN Sales.Invoices AS Inv ON Inv.OrderID = ord.OrderID 
@@ -42,6 +44,20 @@ WHERE ordTotal.CustomerID = Inv.CustomerID
 AND DATEDIFF(dd, Inv.InvoiceDate, ord.OrderDate) = 0 
 GROUP BY ord.CustomerID, det.StockItemID 
 ORDER BY ord.CustomerID, det.StockItemID
+/*
+set statistics io on
+Table 'StockItemTransactions'. Scan count 1, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 29, lob physical reads 0, lob read-ahead reads 0.
+Table 'StockItemTransactions'. Segment reads 1, segment skipped 0.
+Table 'OrderLines'. Scan count 4, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 331, lob physical reads 0, lob read-ahead reads 0.
+Table 'OrderLines'. Segment reads 2, segment skipped 0.
+Table 'Worktable'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'Worktable'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'CustomerTransactions'. Scan count 5, logical reads 261, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'Orders'. Scan count 2, logical reads 883, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'Invoices'. Scan count 1, logical reads 216184, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'StockItems'. Scan count 1, logical reads 2, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+*/
+
 
 --Шаг 2
 /*
@@ -67,6 +83,7 @@ select count(*) from  Sales.Orders
 /*
 Заменил подзапрос с вычислением списка лучших клиентов
 --Пример 1
+set statistics io on
 ;WITH CustomersCTE(CustomerID, summ) AS (
 	SELECT o.CustomerID, 
 	SUM(ol.UnitPrice * ol.Quantity) OVER(PARTITION BY CustomerID)  
@@ -80,8 +97,14 @@ on o.CustomerID = c.CustomerID
 where summ>250000
 --запрос выполняется долго и высокая сложность ~2.8
 
+--Table 'OrderLines'. Scan count 2, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 163, lob physical reads 0, lob read-ahead reads 0.
+--Table 'OrderLines'. Segment reads 1, segment skipped 0.
+--Table 'Worktable'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+--Table 'Orders'. Scan count 2, logical reads 382, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+
 
 --Пример 2
+set statistics io on
 ;WITH CustomersCTE(CustomerID) AS (
 	SELECT o.CustomerID FROM Sales.OrderLines ol
 	INNER JOIN Sales.Orders o ON ol.OrderID = o.OrderID
@@ -89,11 +112,19 @@ where summ>250000
 	HAVING SUM(ol.UnitPrice * ol.Quantity) > 250000
 )
 select CustomerID from CustomersCTE
+--Table 'OrderLines'. Scan count 2, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 163, lob physical reads 0, lob read-ahead reads 0.
+--Table 'OrderLines'. Segment reads 1, segment skipped 0.
+--Table 'Worktable'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+--Table 'Orders'. Scan count 1, logical reads 191, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+
 --Сложность 0.48. 
 Вывод: Лучше оптимизатора сделать не удалось
+В примере №2 меньше работа с таблицей Orders
 
 */
 
+
+set statistics io on
 ;WITH CustomersCTE(CustomerID) AS (
 	SELECT o.CustomerID FROM Sales.OrderLines ol
 	INNER JOIN Sales.Orders o ON ol.OrderID = o.OrderID
@@ -112,8 +143,65 @@ AND
 AND DATEDIFF(dd, Inv.InvoiceDate, ord.OrderDate) = 0 
 GROUP BY ord.CustomerID, det.StockItemID 
 ORDER BY ord.CustomerID, det.StockItemID
+/*
+Table 'StockItemTransactions'. Scan count 1, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 29, lob physical reads 0, lob read-ahead reads 0.
+Table 'StockItemTransactions'. Segment reads 1, segment skipped 0.
+Table 'OrderLines'. Scan count 4, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 331, lob physical reads 0, lob read-ahead reads 0.
+Table 'OrderLines'. Segment reads 2, segment skipped 0.
+Table 'Worktable'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'Worktable'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'CustomerTransactions'. Scan count 5, logical reads 261, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'Invoices'. Scan count 54863, logical reads 277896, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'Orders'. Scan count 2, logical reads 883, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'StockItems'. Scan count 1, logical reads 2, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Меньше обращений к таблице Invoices стало с 
+*/
+
+--Шаг5
+/*
+В выводе статистике большие значения для таблицы Invoices в полях Scan count 54863 и  logical reads 277896.
+Scan count- это количество сканирований таблицы/индекса. Соответственно решено проверить наличие индексов для условий where в таблице Invoices, так и для условий при слоединениях.
+*/
+--Запрос соедения Invoices
+select s.CustomerID
+from Sales.Invoices i inner join sales.Orders s 
+on i.OrderID = s.OrderID
+WHERE i.BillToCustomerID <> s.CustomerID 
+/*юез условия where sql сервер выбирает merge Оператор для условия on i.OrderID = s.OrderID/
+А вот i.BillToCustomerID <> s.CustomerID  показывает Hash с примерной стоимостью оператора ~10.1 sql попугай.
+После добавления доп поля BillToCustomerID в индекс FK_Sales_Invoices_OrderID выборка стала показывать оператор merge и стоимость 1.17 
+*/
+--ниже запрос перестроения через удаления и добавление индекса
+USE [WideWorldImporters]
+GO
+DROP INDEX [FK_Sales_Invoices_OrderID] ON [Sales].[Invoices]
+GO
+CREATE NONCLUSTERED INDEX [FK_Sales_Invoices_OrderID] ON [Sales].[Invoices]
+(
+	[OrderID] ASC,
+	[BillToCustomerID] ASC
+) 
+GO
+--Шаг6 
+/*
+Проверка запроса из Шага4 после добавления поля в индекс в шаге 5
+*/
+/*
+Table 'OrderLines'. Scan count 4, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 331, lob physical reads 0, lob read-ahead reads 0.
+Table 'OrderLines'. Segment reads 2, segment skipped 0.
+Table 'Worktable'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'StockItemTransactions'. Scan count 3619, logical reads 25105, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'Worktable'. Scan count 0, logical reads 0, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'CustomerTransactions'. Scan count 5, logical reads 261, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'Invoices'. Scan count 11767, logical reads 48242, physical reads 0, read-ahead reads 2, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'Orders'. Scan count 2, logical reads 883, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+Table 'StockItems'. Scan count 1, logical reads 2, physical reads 0, read-ahead reads 0, lob logical reads 0, lob physical reads 0, lob read-ahead reads 0.
+*/
+--После добавления BillToCustomerID в FK_Sales_Invoices_OrderID число Scan count для таблицы Invoices упало с 54863 до 11767, т.е. меньше обращений к индексу при поиске
+
 
 /*
 1) По плану выполнения сложность с 7.5 упала до 5.3 за счет преварительной склейки данных.
 2) Добавления грязного чтения set transaction isolation level read uncommitted не улучшает statistics time on, но годится для уменьшая блокировок при чтении (Shared) если разрешено бизнес логикой
+3) Добавления доп поля в индексе сокращает количества операций для поиска по индеквсу
 */
